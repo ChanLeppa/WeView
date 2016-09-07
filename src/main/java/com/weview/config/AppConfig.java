f3b189bcf927f1a47381e65c5a7a02823c9bb9b1
HA999 <hananavramovich@gmail.com>
2016-09-08 01:41:17 +0300

    REDIS_URL

--- src/main/java/com/weview/config/AppConfig.java
+++ src/main/java/com/weview/config/AppConfig.java
@@ -7,18 +7,28 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Configuration
 @Import(WebSocketConfig.class)
 public class AppConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
+        URI redisURI = null;
+        try {
+            redisURI = new URI(System.getenv("REDIS_URL"));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
         JedisConnectionFactory factory = new JedisConnectionFactory();
-//        factory.setHostName("ec2-54-247-91-92.eu-west-1.compute.amazonaws.com");
-//        factory.setPassword("pbes6ltvi1464584if23s18u4qt");
-//        factory.setPort(13389);
-//        factory.setUsePool(true);
-//        factory.afterPropertiesSet();
+        factory.setHostName(redisURI.getHost());
+//        factory.setHostName("redis://h:pbes6ltvi1464584if23s18u4qt@ec2-54-247-91-92.eu-west-1.compute.amazonaws.com:13389");
+        factory.setPassword("pbes6ltvi1464584if23s18u4qt");
+        factory.setPort(13389);
+        factory.setUsePool(true);
+        factory.afterPropertiesSet();
         return factory;
     }
 
