88d632dcc9ab0635df31910d645fd860910030a3
HA999 <hananavramovich@gmail.com>
2016-09-11 11:43:22 +0300

    Redis connection for heroku

--- src/main/java/com/weview/config/AppConfig.java
+++ src/main/java/com/weview/config/AppConfig.java
@@ -17,8 +17,8 @@ public class AppConfig {
 //        factory.setHostName("ec2-54-247-91-92.eu-west-1.compute.amazonaws.com");
 //        factory.setPassword("pbes6ltvi1464584if23s18u4qt");
 //        factory.setPort(13389);
-//        factory.setUsePool(true);
-//        factory.afterPropertiesSet();
+        factory.setUsePool(true);
+        factory.afterPropertiesSet();
         return factory;
     }
 
